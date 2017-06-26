91f219665ae70a93042bc1b4bbffc95d197adb69
Philip Graf <git@acanda.ch>
2017-06-26 16:54:14 +0200

    Improve model and refactor code a bit

--- convolution_train.py
+++ convolution_train.py
@@ -9,10 +9,13 @@ from keras.losses import categorical_crossentropy
 from keras.optimizers import Adadelta
 from keras.callbacks import TensorBoard
 
-
 input_path = "input"
 poster_width = 48  # 182 / 3.7916
 poster_height = 64  # 268 / 4.1875
+poster_channels = 3  # RGB
+
+epochs = 2
+batch_size = 256
 
 
 def poster_file(movie):
@@ -55,8 +58,9 @@ def encode(series, uniques):
     return [bitmap(lst, uniques) for lst in series]
 
 
-def load_data():
-    movies = pd.read_csv(input_path + "/MovieGenre.csv", encoding="ISO-8859-1", usecols=['imdbId', 'Genre'], keep_default_na=False)
+def load_data(path):
+    csv = path + "/MovieGenre.csv"
+    movies = pd.read_csv(csv, encoding="ISO-8859-1", usecols=['imdbId', 'Genre'], keep_default_na=False)
     movies = movies[movies.apply(lambda d: has_genre(d) and has_poster(d), axis=1)]
     movies = movies.sample(frac=1).reset_index(drop=True)
     posters = list(map(movie_poster, movies.values))
@@ -67,52 +71,56 @@ def load_data():
     return x, y, unique_genres
 
 
-x_data, y_data, genres = load_data()
+def create_cnn(height, width, channels, genres):
+    cnn = Sequential([
+        Conv2D(filters=16, kernel_size=(5, 5), activation="relu", input_shape=(height, width, channels)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Conv2D(filters=32, kernel_size=(5, 5), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+        Dense(len(genres), activation='softmax')
+    ])
+    cnn.compile(loss=categorical_crossentropy,
+                optimizer=Adadelta(),
+                metrics=['accuracy'])
+    return cnn
+
+
+x_data, y_data, movie_genres = load_data(input_path)
 separator = len(x_data) * 3 // 4
 x_train = x_data[0:separator]
 y_train = y_data[0:separator]
-x_test = x_data[separator:len(x_data)]
-y_test = y_data[separator:len(y_data)]
+x_validation = x_data[separator:len(x_data)]
+y_validation = y_data[separator:len(y_data)]
 
-model = Sequential([
-    Conv2D(filters=128, kernel_size=(4, 4), activation="relu", input_shape=(poster_height, poster_width, 3)),
-    Conv2D(filters=64, kernel_size=(4, 4), activation='relu'),
-    MaxPooling2D(pool_size=(2, 2)),
-    Dropout(0.25),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dropout(0.5),
-    Dense(len(genres), activation='softmax')
-])
-
-model.compile(loss=categorical_crossentropy,
-              optimizer=Adadelta(),
-              metrics=['accuracy'])
+model = create_cnn(poster_height, poster_width, poster_channels, movie_genres)
+model.summary()
 
 tensorboard = TensorBoard(write_images=True)
 
-epochs = 5
-batch_size = 256
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test),
+                    validation_data=(x_validation, y_validation),
                     callbacks=[tensorboard])
 
 model.save(f"models/conv-model-e{epochs}-b{batch_size}.h5")
 model.save_weights(f"models/conv-weights-e{epochs}-b{batch_size}.h5")
 
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model.evaluate(x_validation, y_validation, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 print("\nInput (from training data):", y_data[0:3])
 print("Prediction:", model.predict(x_data[0:3]))
 
-print("\nInput (from test data):", y_data[-4:-1])
+print("\nInput (from validation data):", y_data[-4:-1])
 print("Prediction:", model.predict(x_data[-4:-1]))
 
-print("\nGenres:", genres)
+print("\nGenres:", movie_genres)
 
 keras.utils.plot_model(model, to_file=f"models/convolution.png", show_shapes=True)
